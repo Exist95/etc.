1d06f661743ef58157037e65e4ae29deaeba506c
Exist95 <96723716+Exist95@users.noreply.github.com>
2022-01-07 21:07:09 +0900

    update

---
+++ array-loop.js
@@ -0,0 +1,8 @@
+var number = [1,400,12,34,5];
+var i = 0;
+while(i < number.length){ //i < 5 = 정적, i < number.length = 동적!
+  total = total + neumber[i];
+  console.log(number[i]);
+  i = i = 1;
+}
+console.log(`total : ${total}`)
---
+++ array.js
@@ -0,0 +1,9 @@
+//CRUD
+var arr = ['A','B','C','D'];
+console.log(arr[1]);
+console.log(arr[3]);
+arr[2] = 3; //수정하는 법
+console.log(arr);
+console.log(arr.length);
+arr.push('E');
+console.log(arr);
