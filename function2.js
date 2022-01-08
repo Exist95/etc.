60e07066a95777e52f9b08147d247bb066bc8e8d
Exist95 <96723716+Exist95@users.noreply.github.com>
2022-01-08 20:30:50 +0900

    function2.js

---
+++ function2.js
@@ -0,0 +1,8 @@
+console.log(Math.round(1.6)); //2         라운드 = 반올림
+console.log(Math.round(1.4)); //1
+
+function sum(first, second){  // 매개변수 = parameter라고 부름
+  console.log(first+second);
+}
+
+ sum(2,4); // 값 하나 하나를 arqument라고 함
