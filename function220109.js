07da33f14acb339c82cf96ada68512f527bee0c3
Exist95 <96723716+Exist95@users.noreply.github.com>
2022-01-12 19:48:59 +0900

    Add files via upload

---
+++ function220109.js
@@ -0,0 +1,15 @@
+console.log(Math.round(1.6)); //2         라운드 = 반올림
+console.log(Math.round(1.4)); //1
+
+//------------------------------------------------------
+
+function sum(first, second){  // 매개변수 = parameter라고 부름
+  console.log('a');
+  return first+second;
+  console.log('b');
+}
+
+console.log(sum(2,4)); // 값 하나 하나를 arqument라고 함
+//------------------------------------------------------
+
+console.log(Math.round(1.6));
